fde1e26437d49c2f60c843c7f4446b21d7abbcf0
DefenseArt <82384826+DefenseArt@users.noreply.github.com>
2025-02-21 14:45:05 +0900

    Update memo.js

--- code/memo/memo.js
+++ code/memo/memo.js
@@ -2,6 +2,7 @@ const memoInput = document.getElementById("memoInput");
 const addBtn = document.getElementById("addMemoBtn");
 const memoList = document.getElementById("memoList");
 
+
 memoInput.addEventListener("keydown", (event) => {
   if (event.key === "Enter") 
     addBtn.click();
@@ -10,10 +11,26 @@ memoInput.addEventListener("keydown", (event) => {
 addBtn.addEventListener("click", () => {
   if(memoInput.value.trim() === "") return; // 빈 값 방지
 
-  const mlist = document.createElement("li");
-  mlist.innerText = memoInput.value;
+  let memos = JSON.parse(localStorage.getItem("memos")) || [];
+
+  memos.push(memoInput.value);
   
-  memoList.appendChild(mlist);
+  localStorage.setItem("memos", JSON.stringify(memos));
+
+  renderMemos();
 
   memoInput.value = ""; // 입력 창 초기화
 });
+
+function renderMemos() {
+  memoList.innerHTML = "";
+  const memos = JSON.parse(localStorage.getItem("memos")) || [];
+
+  memos.forEach((memo) => {
+    const li =document.createElement("li");
+    li.innerText = memo;
+    memoList.appendChild(li);
+  });
+}
+
+document.addEventListener("DOMContentLoaded", renderMemos);
