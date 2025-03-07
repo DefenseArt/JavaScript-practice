f4c7982314caab1ef02fbbbbc6df0e291ffb57cc
DefenseArt <82384826+DefenseArt@users.noreply.github.com>
2025-03-07 17:47:50 +0900

    Update calculator.js

--- code/calculator/calculator.js
+++ code/calculator/calculator.js
@@ -6,39 +6,42 @@ const mul = document.getElementById("multiply");
 const div = document.getElementById("divide");
 const result = document.getElementById("result");
 
-
 function getNumbers() {
-  const num1input = parseFloat(num1.value);
-  const num2input = parseFloat(num2.value);
-
-  if (isNaN(num1input) || isNaN(num2input)) {
-    result.innerText = "숫자를 입력하세요!";
+  if (num1.value.trim() === "") {
+    result.innerText = "첫 번째 입력 값이 비었는지 확인";
     return null;
   }
-  return { num1input, num2input };
+  if(num2.value.trim() === "")
+  {
+    result.innerText = "두 번째 입력 값이 비었는지 확인";
+    return null;
+  }
+  return { num1, num2 };
 }
 
 //더하기
 add.addEventListener("click", () => {
  
   const numbers = getNumbers();
-  if (numbers)  result.innerText = "결과: " + (numbers.num1input + numbers.num2input);
+  if (numbers)  result.innerText = "결과: " + (numbers.num1 + numbers.num2);
 });
 
 //빼기
 sub.addEventListener("click", () => {
   const numbers = getNumbers();
-  if (numbers)  result.innerText = "결과: " + (numbers.num1input - numbers.num2input);
+  if (numbers)  result.innerText = "결과: " + (numbers.num1 - numbers.num2);
 });
 
 //곱하기
 mul.addEventListener("click", () => { 
   const numbers = getNumbers();
-  if (numbers) result.innerText = "결과: " + (numbers.num1input * numbers.num2input);
+  if (numbers) result.innerText = "결과: " + (numbers.num1 * numbers.num2);
 });
 
 //나누기
 div.addEventListener("click", () => { 
   const numbers = getNumbers();
-  if (numbers) result.innerText = "결과: " + (numbers.num1input / numbers.num2input);
+  if(numbers.num2 === 0)
+    result.innerText = "0으로 나눌 수 없습니다.";
+  if (numbers) result.innerText = "결과: " + (numbers.num1 / numbers.num2).toFixed(2);
 });
